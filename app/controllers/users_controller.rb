cec1944d2b4069ac479ee9de9fbaa0710528eaa5
drapes <tihon1997@gmail.com>
2017-05-05 15:35:56 +0300

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,44 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+        User.new(
+          id: 1,
+          name: 'Tihon',
+          username: 'tantskii',
+          avatar_url: 'mable.png'
+        ),
+        User.new(
+          id: 2,
+          name: 'Pablo',
+          username: 'brtbds',
+        ),
+        User.new(
+          id: '3',
+          name: 'Ilya',
+          username: 'oldflanigan',
+          avatar_url: 'soos.png'
+        )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Привет"
+    @user = User.new(
+        name: 'Tihon',
+        username: 'tantskii',
+        avatar_url: 'mable.png'
+    )
+
+    @questions = [
+        Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+        Question.new(text: 'Что делаешь?', created_at: Date.parse('29.03.2017'))
+    ]
+
+    @new_question = Question.new
   end
 end
